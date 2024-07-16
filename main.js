8098878b81a59dde2eee6e864f9a9b06a0df8d37
jungminji <wjdalswl0215@gmail.com>
2024-07-16 16:58:11 +0900

    firebase 설정

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+// Firebase SDK 라이브러리 가져오기
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import {
+  collection,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { getDocs } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+// Firebase 구성 정보 설정
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDiWlH_rkm_DZoo5ppQDqpCg5o-1hgoGNU",
+  authDomain: "todolist-a1b7b.firebaseapp.com",
+  projectId: "todolist-a1b7b",
+  storageBucket: "todolist-a1b7b.appspot.com",
+  messagingSenderId: "416626392704",
+  appId: "1:416626392704:web:7215c827e0cdeb1a597065",
+  measurementId: "G-71C9PBMBK8",
+};
+
+// Firebase 인스턴스 초기화
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 // 추가 버튼을 누르면 할일 리스트에 추가된다.
 document.getElementById("add-task").addEventListener("click", addTask);
 
